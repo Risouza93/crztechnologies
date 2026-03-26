862b58c5b86439706e608e7ca3438e5423e0689a
Richard Souza <risouza93@gmail.com>
2026-03-26 11:59:27 -0300

    Adding JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// pega o botão pelo ID
+const botao = document.getElementById("btnContato");
+
+// adiciona ação de clique
+botao.addEventListener("click", () => {
+  alert("Vamos conversar! 🚀");
+});
