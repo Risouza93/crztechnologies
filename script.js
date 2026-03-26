346f2dd3b6d9852775c601c98560688ecb321557
Richard Souza <risouza93@gmail.com>
2026-03-26 12:33:36 -0300

    rollback

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-//SEÇÕES
+// pega o botão pelo ID
+const botao = document.getElementById("btnContato");
 
-//Formulário de contato
-
-const form = document.getElementById("formContato");
-const mensagem = document.getElementById("mensagemStatus");
-const btnContato = document.querySelector("button");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const nome = document.querySelector("input[type='text']").value;
-  const email = document.querySelector("input[type='email']").value;
-
-  // validação
-  if (nome === "" || email === "") {
-    mensagem.textContent = "⚠️ Preencha todos os campos!";
-    mensagem.className = "erro";
-    return;
-  }
-
-  // estado loading
-  btnContato.classList.add("loading");
-  btnContato.textContent = "Enviando...";
-  btnContato.disabled = true;
-
-  mensagem.textContent = "";
-  
-  // simulação de envio (2 segundos)
-  setTimeout(() => {
-    mensagem.textContent = "✅ Mensagem enviada com sucesso!";
-    mensagem.className = "sucesso";
-
-    btnContato.classList.remove("loading");
-    btnContato.textContent = "Enviar mensagem";
-    btnContato.disabled = false;
-
-    form.reset();
-  }, 2000);
+// adiciona ação de clique
+botao.addEventListener("click", () => {
+  document.getElementById("contato").scrollIntoView({
+    behavior: "smooth"
 });
+});
+
+
