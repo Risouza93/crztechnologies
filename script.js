e8753b6680a870c641856610e3003374c7c4fc81
Richard Souza <risouza93@gmail.com>
2026-03-26 12:17:12 -0300

    UX

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ botao.addEventListener("click", () => {
     behavior: "smooth"
 });
 });
+
+//Formulário de contato
+
+const form = document.getElementById("formContato");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // impede recarregar a página
+
+  const nome = document.querySelector("input[type='text']").value;
+  const email = document.querySelector("input[type='email']").value;
+
+  if (nome === "" || email === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  alert("Mensagem enviada com sucesso! 🚀");
+});
+
+//SEÇÕES*
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    const top = sec.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 50) {
+      sec.classList.add("show");
+    }
+  });
+});
