e82ecabd52adec2a71979f6b46d756146d7e9794
Richard Souza <risouza93@gmail.com>
2026-03-27 01:14:48 -0300

    fix: adiciona Sobre, Projetos e Stack placeholder

---
+++ my-app/src/components/Projetos.tsx
@@ -0,0 +1,10 @@
+const Projetos = () => {
+  return (
+    <div>
+      <h2>Projetos</h2>
+      <p>Em breve.</p>
+    </div>
+  );
+};
+
+export default Projetos;
---
+++ my-app/src/components/Sobre.tsx
@@ -0,0 +1,10 @@
+const Sobre = () => {
+  return (
+    <div>
+      <h2>Sobre</h2>
+      <p>Em breve.</p>
+    </div>
+  );
+};
+
+export default Sobre;
